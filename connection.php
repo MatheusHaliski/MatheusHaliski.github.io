beff477b0d520799a78d015791f733225d6ffa12
MatheusHaliski <matheushaliski@gmail.com>
2024-02-07 14:25:52 -0300

    as
    as

--- connection.php
+++ connection.php
@@ -2,25 +2,20 @@
     $dbServerName = 'localhost';
     $dbUserName = 'root';
     $dbPassword = '';
-    $dbName = 'first';
+    $dbName = 'Regionais';
 
-    $connection = new mysqli($dbServerName, $dbUserName, $dbPassword, $dbName);
-    if($connection->connect_error) {
-        echo "$connection->connect_error";
-        die("Connection failed: " . $connection->connect_error);
-    }
-    if(isset($_POST['cadastro'])) {
-            $name = mysqli_real_escape_string($connection, $_POST['name']);
-            $cpf = mysqli_real_escape_string($connection, $_POST['cpf']);
-
-            $sql = "INSERT INTO user (name, cpf) VALUES ('$name', '$cpf')";
-mysqli_query($connection,$sql);
-    if(mysqli_query($connection, $sql)) {
-                echo 'Success!';
-            } else {
-                echo 'Error! Could not able to execute $sql' . mysqli_error($connection);
-            }
-        }
-
-    mysqli_close($connection);
+$connection = new mysqli($dbServerName, $dbUserName, $dbPassword, $dbName);
+$sqe = "SELECT * FROM ServidoresCascavel";
+$sqe1 = "SELECT * FROM ServidoresCampoMourao";
+$sqe2 = "SELECT * FROM ServidoresCuritiba";
+$sqe2A = "SELECT * FROM ServidoresGuarapuava";
+$sqe3 = "SELECT * FROM ServidoresLondrina";
+$sqe4 = "SELECT * FROM ServidoresMaringa";
+$sqe5 = "SELECT * FROM ServidoresPatoBranco";
+$sqe6 = "SELECT * FROM ServidoresPontaGrossa";
+$sqe7 = "SELECT * FROM ServidoresSantoAntoniodaPlatina";
+$sqe8 = "SELECT * FROM ServidoresUmuarama";
+$sqe9 = "SELECT * FROM ServidoresUniaodaVitoria";
+$ffaf = "SELECT ID AS 'ID',name AS 'Funcionario',DATE_FORMAT(age,'%d-%m-%Y') AS 'data_formatada' ,departament AS 'dep' 
+FROM data; ";
 ?>
